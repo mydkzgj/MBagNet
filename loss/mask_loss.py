ef986ce5f4d64a2a4460acfc0e0aec1f6106a178
Jack <sychenjiayang@163.com>
2020-03-21 15:39:04 +0800

    p+n

--- loss/mask_loss.py
+++ loss/mask_loss.py
@@ -343,7 +343,7 @@ class GradCamMaskLoss(object):
             # a = torch.isnan(pos_loss)
             # if a.item() == 1:
             #    print("Nan")
-            total_loss_list.append(pos_loss)
+            total_loss_list.append(pos_loss+neg_loss)
 
         while len(total_loss_list) < 4:
             total_loss_list.append(0)
