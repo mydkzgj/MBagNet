fad2ec8917480f17dd2969c21a13a7932fc8f027
Jack <sychenjiayang@163.com>
2020-03-22 14:21:55 +0800

    resize+s0.5+pos

--- modeling/baseline.py
+++ modeling/baseline.py
@@ -219,7 +219,7 @@ class Baseline(nn.Module):
             self.projectors = torch.nn.Conv2d(1,1,kernel_size=1,bias=False)
             nn.init.constant_(self.projectors.weight, 1)
 
-            self.target_layer = ["denseblock1", "denseblock2", "denseblock3"]#"conv0"#"denseblock3"#"conv0"#"denseblock1"  "denseblock2", "denseblock3",
+            self.target_layer = ["denseblock4"]#"conv0"#"denseblock3"#"conv0"#"denseblock1"  "denseblock2", "denseblock3",
             #"denseblock1", "denseblock2", "denseblock3",
             if self.target_layer != []:
                 for tl in self.target_layer:
