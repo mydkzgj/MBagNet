b7845de2cc481c2def1eabad9361a50c5919fb99
Jack <sychenjiayang@163.com>
2020-04-03 20:17:42 +0800

    1

--- engine/trainer.py
+++ engine/trainer.py
@@ -322,9 +322,9 @@ def create_supervised_trainer(model, optimizers, metrics, loss_fn, device=None,)
             elif model.maskedImgReloadType == "seg_gtmask":
                 soft_mask = seg_gt_masks
                 soft_mask = model.lesionFusion(soft_mask, labels[labels.shape[0]-soft_mask.shape[0]:labels.shape[0]])
-                max_kernel_size = random.randint(80, 320)
+                max_kernel_size = random.randint(40, 320)
                 soft_mask = torch.nn.functional.max_pool2d(soft_mask, kernel_size=max_kernel_size*2+1, stride=1, padding=max_kernel_size)
-                soft_mask = torch.nn.functional.avg_pool2d(soft_mask, kernel_size=40, stride=1, padding=20)
+                soft_mask = torch.nn.functional.avg_pool2d(soft_mask, kernel_size=41, stride=1, padding=20)
             elif model.maskedImgReloadType == "joint":
                 if model.segmentationType != "denseFC":
                     raise Exception("segmentationType can't match maskedImgReloadType")
