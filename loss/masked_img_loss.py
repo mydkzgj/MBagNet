7c65cf68652a24183d79a4cd0a9b0cd5b1ca0e40
Jack <sychenjiayang@163.com>
2020-04-05 20:23:23 +0800

    1

--- loss/masked_img_loss.py
+++ loss/masked_img_loss.py
@@ -115,13 +115,13 @@ class PosMaskedImgLoss(object):
         # CJY distribution 1  cross_entropy_loss min
         # pos_masked区域的img应该更容易区分类别
         reload_label = label[label.shape[0] - pos_masked_logits.shape[0]:label.shape[0]]
-        #loss = F.cross_entropy(pos_masked_logits, reload_label, reduction="none")
+        loss = F.cross_entropy(pos_masked_logits, reload_label, reduction="none")
 
-        reload_label = label[label.shape[0] - neg_masked_logits.shape[0]:label.shape[0]]
-        one_hot_label = torch.nn.functional.one_hot(reload_label, pos_masked_logits.shape[1]).float()
-        score = F.softmax(pos_masked_logits, dim=1)
-        score = -torch.log(score)
-        loss = score[one_hot_label.bool()]
+        #reload_label = label[label.shape[0] - neg_masked_logits.shape[0]:label.shape[0]]
+        #one_hot_label = torch.nn.functional.one_hot(reload_label, pos_masked_logits.shape[1]).float()
+        #score = F.softmax(pos_masked_logits, dim=1)
+        #score = -torch.log(score)
+        #loss = score[one_hot_label.bool()]
 
         # 挑选指定sample的loss
         pick_index = torch.ne(reload_label, -1) & torch.ne(reload_label, 5)  & torch.ne(reload_label, 3) & torch.ne(reload_label, 4)#& torch.ne(label, 0)
