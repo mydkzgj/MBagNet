e715452e2093291782b80dc1f498f60009c00670
Jack <sychenjiayang@163.com>
2020-04-06 09:45:52 +0800

    1

--- engine/trainer.py
+++ engine/trainer.py
@@ -178,7 +178,7 @@ def create_supervised_trainer(model, optimizers, metrics, loss_fn, device=None,)
 
         # Master 0 运行模型  (内置运行 Branch 1 Segmentation)
         # 设定有多少样本需要进行支路的运算
-        """
+        #"""
         model.transimitBatchDistribution((grade_num+seg_num-model.branch_img_num, model.branch_img_num))
         model.transmitClassifierWeight()   #如果是BOF 会回传分类器权重
         logits = model(imgs)               #为了减少显存，还是要区分grade和seg
@@ -186,14 +186,14 @@ def create_supervised_trainer(model, optimizers, metrics, loss_fn, device=None,)
         #"""
 
         # 提前进行样本扩增  CJY at 2020.4.5
-        #"""
+        """
         soft_mask = seg_gt_masks
         soft_mask = model.lesionFusion(soft_mask, labels[labels.shape[0] - soft_mask.shape[0]:labels.shape[0]])
-        max_kernel_size = random.randint(20, 320)
+        max_kernel_size = random.randint(20, 40)
         soft_mask = torch.nn.functional.max_pool2d(soft_mask, kernel_size=max_kernel_size * 2 + 1, stride=1,
                                                    padding=max_kernel_size)
 
-        rimgs = imgs[imgs.shape[0] - soft_mask.shape[0]:imgs.shape[0]]
+        rimgs = imgs[imgs.shape[0] - soft_mask.shape[0]:imgs.shape[0]].clone()
         rimg_mean = rimgs.mean(-1, keepdim=True).mean(-2, keepdim=True)
         pos_masked_img = soft_mask * rimgs  # + (1-soft_mask) * rimg_mean
         neg_masked_img = (1 - soft_mask) * rimgs  # + soft_mask * rimg_mean
@@ -371,10 +371,10 @@ def create_supervised_trainer(model, optimizers, metrics, loss_fn, device=None,)
                 pass
 
             # (2).生成masked_img
-            #rimgs = imgs[imgs.shape[0]-soft_mask.shape[0]:imgs.shape[0]]
-            #rimg_mean = rimgs.mean(-1, keepdim=True).mean(-2,keepdim=True)
-            #pos_masked_img = soft_mask * rimgs #+ (1-soft_mask) * rimg_mean
-            #neg_masked_img = (1-soft_mask) * rimgs #+ soft_mask * rimg_mean
+            rimgs = imgs[imgs.shape[0]-soft_mask.shape[0]:imgs.shape[0]]
+            rimg_mean = rimgs.mean(-1, keepdim=True).mean(-2,keepdim=True)
+            pos_masked_img = soft_mask * rimgs #+ (1-soft_mask) * rimg_mean
+            neg_masked_img = (1-soft_mask) * rimgs #+ soft_mask * rimg_mean
 
             # (3).reload maskedImg
             # 使用参数相同的网络，但是不回传
