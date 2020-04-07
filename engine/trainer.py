00e83fdcdbf2f99c74eafb0a4c0ea4cd27f1a696
Jack <sychenjiayang@163.com>
2020-04-07 19:54:35 +0800

    1

--- engine/trainer.py
+++ engine/trainer.py
@@ -189,20 +189,19 @@ def create_supervised_trainer(model, optimizers, metrics, loss_fn, device=None,)
         #"""
         soft_mask = seg_gt_masks
         soft_mask = model.lesionFusion(soft_mask, labels[labels.shape[0] - soft_mask.shape[0]:labels.shape[0]])
-        #soft_mask = 1 - soft_mask
 
-        max_kernel_size = random.randint(30, 240)
+        max_kernel_size = 30#random.randint(30, 240)
         soft_mask = torch.nn.functional.max_pool2d(soft_mask, kernel_size=max_kernel_size * 2 + 1, stride=1,
                                                    padding=max_kernel_size)
 
         rimgs = imgs[imgs.shape[0] - soft_mask.shape[0]:imgs.shape[0]].clone()
         rimg_mean = rimgs.mean(-1, keepdim=True).mean(-2, keepdim=True)
         pos_masked_img = soft_mask * rimgs  # + (1-soft_mask) * rimg_mean
-        #neg_masked_img = (1 - soft_mask) * rimgs  # + soft_mask * rimg_mean
+        neg_masked_img = (1 - soft_mask) * rimgs  # + soft_mask * rimg_mean
 
-        for i in range(grade_num):
-            if labels[i] == 0:
-                neg_masked_img = imgs[i:i + 1] * soft_mask
+        #for i in range(grade_num):
+        #    if labels[i] == 0:
+        #        neg_masked_img = imgs[i:i + 1] * soft_mask
 
         #imgs[imgs.shape[0] - soft_mask.shape[0]:imgs.shape[0]] = pos_masked_img
         imgs = torch.cat([imgs, pos_masked_img, neg_masked_img])
@@ -223,13 +222,13 @@ def create_supervised_trainer(model, optimizers, metrics, loss_fn, device=None,)
         m_logits = logits[logits.shape[0]-rimgs.shape[0]*3:logits.shape[0]]
         om_logits = m_logits[0:m_logits.shape[0] // 3]
         pm_logits = m_logits[m_logits.shape[0] // 3:m_logits.shape[0] // 3 * 2]
-        nm_logits = m_logits[m_logits.shape[0] // 3 * 2:m_logits.shape[0]]
-        #logits = logits[0:grade_num+seg_num]
+        nm_logits = None#m_logits[m_logits.shape[0] // 3 * 2:m_logits.shape[0]]
+        logits = logits[0:grade_num+seg_num]
 
-        om_labels = labels[labels.shape[0]-rimgs.shape[0]:labels.shape[0]]
-        pm_labels = om_labels
-        nm_labels = om_labels * 0
-        labels = torch.cat([labels, pm_labels, nm_labels], dim=0)
+        #om_labels = labels[labels.shape[0]-rimgs.shape[0]:labels.shape[0]]
+        #pm_labels = om_labels
+        #nm_labels = om_labels * 0
+        #labels = torch.cat([labels, pm_labels, nm_labels], dim=0)
         #"""
 
 
@@ -370,10 +369,11 @@ def create_supervised_trainer(model, optimizers, metrics, loss_fn, device=None,)
                 w = 8
                 soft_mask = torch.sigmoid(w * (overall_gcam - sigma))  # overall_gcam [0,1]
             elif model.maskedImgReloadType == "seg_gtmask":
-                soft_mask = seg_gt_masks
-                soft_mask = model.lesionFusion(soft_mask, labels[labels.shape[0]-soft_mask.shape[0]:labels.shape[0]])
-                max_kernel_size = random.randint(30, 240)
-                soft_mask = torch.nn.functional.max_pool2d(soft_mask, kernel_size=max_kernel_size*2+1, stride=1, padding=max_kernel_size)
+                pass
+                #soft_mask = seg_gt_masks
+                #soft_mask = model.lesionFusion(soft_mask, labels[labels.shape[0]-soft_mask.shape[0]:labels.shape[0]])
+                #max_kernel_size = random.randint(30, 240)
+                #soft_mask = torch.nn.functional.max_pool2d(soft_mask, kernel_size=max_kernel_size*2+1, stride=1, padding=max_kernel_size)
                 #soft_mask = torch.nn.functional.avg_pool2d(soft_mask, kernel_size=81, stride=1, padding=40)
                 #soft_mask = 1 - soft_mask
 
@@ -398,7 +398,7 @@ def create_supervised_trainer(model, optimizers, metrics, loss_fn, device=None,)
                 pass
 
             # (2).生成masked_img
-            rimgs = imgs[imgs.shape[0]-soft_mask.shape[0]:imgs.shape[0]]
+            #rimgs = imgs[imgs.shape[0]-soft_mask.shape[0]:imgs.shape[0]]
             #rimg_mean = rimgs.mean(-1, keepdim=True).mean(-2,keepdim=True)
             #pos_masked_img = soft_mask * rimgs #+ (1-soft_mask) * rimg_mean
             #neg_masked_img = (1-soft_mask) * rimgs #+ soft_mask * rimg_mean
