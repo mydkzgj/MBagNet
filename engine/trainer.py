19ccd4d5fb616b32a1ba3b523e772c8789e40793
Jack <sychenjiayang@163.com>
2020-04-14 00:43:24 +0800

    1

--- engine/trainer.py
+++ engine/trainer.py
@@ -274,7 +274,7 @@ def create_supervised_trainer(model, optimizers, metrics, loss_fn, device=None,)
                 # 插值
                 #gcam = torch.nn.functional.interpolate(gcam, (seg_gt_masks.shape[-2], seg_gt_masks.shape[-1]), mode='bilinear')  #mode='nearest'  'bilinear'
                 gcam_list.append(gcam)   #将不同模块的gcam保存到gcam_list中
-                gcam_max_list[i] = pow(gcam_max, 2)
+                gcam_max_list[i] = gcam_max/2
 
             #print("1")
             # 多尺度下的gcam进行融合
