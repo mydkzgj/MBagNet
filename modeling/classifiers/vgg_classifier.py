de862899c6ed7e6d82687fb9ec971a9fcd3f3ae3
Jack <sychenjiayang@163.com>
2020-09-15 14:52:12 +0800

    1

--- modeling/classifiers/vgg_classifier.py
+++ modeling/classifiers/vgg_classifier.py
@@ -28,7 +28,7 @@ class VggClassifier(nn.Module):
 
         self.add_module(
             "main",  nn.Sequential(
-                nn.Linear(512 * 7 * 7, 4096),
+                nn.Linear(in_planes, 4096), #nn.Linear(512 * 7 * 7, 4096),
                 nn.ReLU(),  # True),
                 nn.Dropout(),
                 nn.Linear(4096, 4096),
