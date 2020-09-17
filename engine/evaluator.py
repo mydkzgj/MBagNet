11e36a6dee0964a230593e4cb64e244e872655be
Jack <sychenjiayang@163.com>
2020-09-17 08:54:44 +0800

    1

--- engine/evaluator.py
+++ engine/evaluator.py
@@ -93,8 +93,8 @@ def create_supervised_evaluator(model, metrics, loss_fn, device=None):
             else:
                 # CJY at 2020.9.5
                 scores = torch.sigmoid(logits).round()
-                #regression_logits = model.zoom_ratio * torch.relu(logits)
-                regression_logits = model.regression_linear(model.base.r_feature)
+                regression_logits = model.zoom_ratio * torch.relu(logits)
+                #regression_logits = model.regression_linear(model.base.r_feature)
                 #logits = model.sigmoid_low_th - torch.relu(model.sigmoid_low_th - logits)
 
             return {"logits": logits, "scores":scores, "labels": labels, "multi-labels":one_hot_labels,
