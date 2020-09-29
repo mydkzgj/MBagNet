06e3b8d3807e493e2237e616d8f576b8b90dc68e
Jack <sychenjiayang@163.com>
2020-09-29 22:46:33 +0800

    1

--- data/samplers/triplet_sampler.py
+++ data/samplers/triplet_sampler.py
@@ -40,7 +40,7 @@ class RandomSampler(Sampler):
         #将data_source中的samples依照类别将同类的sample以列表的形式存入字典中
         self.index_dic = defaultdict(list)  #这种字典与普通字典的却别？
         # for single-label and multi-label  at 2020.9.15
-        for index, (_, _, label) in enumerate(self.data_source):
+        for index, (_, label) in enumerate(self.data_source):
             if isinstance(label, int)==True:
                 self.index_dic[label].append(index)
             elif isinstance(label, list)==True:
