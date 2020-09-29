2237001c022ef653e4760426aa5d85f050fdb57b
Jack <sychenjiayang@163.com>
2020-09-29 19:25:47 +0800

    1

--- data/build.py
+++ data/build.py
@@ -13,6 +13,7 @@ from torchvision import transforms
 
 import torch
 from torch.utils.data import DataLoader
+import os
 
 def collate_fn_seg(batch):
     imgs, masks, labels, imgs_path = zip(*batch)
@@ -20,16 +21,59 @@ def collate_fn_seg(batch):
     return torch.stack(imgs, dim=0), torch.stack(masks, dim=0), labels, imgs_path
 
 def collate_fn(batch):
-    imgs, labels, imgs_path, = zip(*batch)
+    if len(batch[0]) == 2:
+        imgs, labels, = zip(*batch)
+        imgs_path = [""] * len(batch)
+    elif len(batch[0]) == 3:
+        imgs, labels, imgs_path, = zip(*batch)
     labels = torch.tensor(labels, dtype=torch.int64)   # for continuous var CJY at 2020.9.5
     return torch.stack(imgs, dim=0), labels, imgs_path
 
-def make_data_loader(cfg, for_train):
-    if cfg.DATA.DATASETS.NAMES == "none" or cfg.TRAIN.DATALOADER.IMS_PER_BATCH == 0:  #如果batch为0，那么就返回空  CJY at 2020.7.12
-        classes_list = ["{}".format(i) for i in range(cfg.MODEL.CLA_NUM_CLASSES)]
-        return None, None, None, classes_list
+def make_data_loader_for_classic_datasets(cfg, for_train):
+    train_transforms = build_transforms(cfg, is_train=for_train)
+    val_transforms = build_transforms(cfg, is_train=False)
+    test_transforms = build_transforms(cfg, is_train=False)
+
+    num_workers = cfg.DATA.DATALOADER.NUM_WORKERS
+    root_path = cfg.DATA.DATASETS.ROOT_DIR
+    # for those have been realized by torchvision
+    if cfg.DATA.DATASETS.NAMES == "cifar10":
+        root_path = os.path.join(root_path, "CIFAR10")
+        train_set = torchvision.datasets.CIFAR10(root=root_path, train=True, download=True, transform=train_transforms)
+        val_set = torchvision.datasets.CIFAR10(root=root_path, train=False, download=True, transform=val_transforms)
+        test_set = val_set
+        classes_list = train_set.classes
+        num_classes = len(train_set.classes)
+
+    train_loader = DataLoader(
+        train_set, batch_size=cfg.TRAIN.DATALOADER.IMS_PER_BATCH,
+        sampler=RandomSampler(train_set, cfg.TRAIN.DATALOADER.CATEGORIES_PER_BATCH,
+                              cfg.TRAIN.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, num_classes,
+                              is_train=for_train),
+        num_workers=num_workers, collate_fn=collate_fn
+    )
+
+    val_loader = DataLoader(
+        val_set, batch_size=cfg.VAL.DATALOADER.IMS_PER_BATCH, shuffle=False, num_workers=num_workers,
+        # CJY at 2019.9.26 为了能够平衡样本
+        sampler=RandomSampler(val_set, cfg.VAL.DATALOADER.CATEGORIES_PER_BATCH,
+                              cfg.VAL.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, num_classes, is_train=False),
+        collate_fn=collate_fn
+    )
+
+    test_loader = DataLoader(
+        test_set, batch_size=cfg.TEST.DATALOADER.IMS_PER_BATCH, shuffle=False, num_workers=num_workers,
+        # CJY at 2019.9.26 为了能够平衡样本
+        sampler=RandomSampler(test_set, cfg.TEST.DATALOADER.CATEGORIES_PER_BATCH,
+                              cfg.TEST.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, num_classes, is_train=False),
+        #collate_fn=collate_fn
+    )
+
+    return train_loader, val_loader, test_loader, classes_list
+
 
     # CJY at 2019.11.20 加入其他非医学图像数据集
+    """
     if cfg.DATA.DATASETS.NAMES in ["cifa10"]:
         BATCH_SIZE = 128  # 批处理尺寸(batch_size)
         transform_train = transforms.Compose([
@@ -56,24 +100,25 @@ def make_data_loader(cfg, for_train):
         # cifar-10的标签
         classes_list = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
         return train_loader, val_loader, test_loader, classes_list
-    elif cfg.DATA.DATASETS.NAMES in ["imagenet"]:   #好像有不同的预处理方式
-        BATCH_SIZE = 64 # 批处理尺寸(batch_size)
-        transform_train = transforms.Compose([#transforms.RandomRotation(30),
-                                               transforms.RandomResizedCrop(224),
-                                               transforms.RandomHorizontalFlip(),
-                                               transforms.ToTensor(),
-                                               transforms.Normalize([0.485, 0.456, 0.406],
-                                                                    [0.229, 0.224, 0.225])])
+    elif cfg.DATA.DATASETS.NAMES in ["imagenet"]:  # 好像有不同的预处理方式
+        BATCH_SIZE = 64  # 批处理尺寸(batch_size)
+        transform_train = transforms.Compose([  # transforms.RandomRotation(30),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])])
 
         transform_test = transforms.Compose([transforms.Resize(256),
-                                              transforms.CenterCrop(224),
-                                              transforms.ToTensor(),
-                                              transforms.Normalize([0.485, 0.456, 0.406],
-                                                                   [0.229, 0.224, 0.225])])
+                                             transforms.CenterCrop(224),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize([0.485, 0.456, 0.406],
+                                                                  [0.229, 0.224, 0.225])])
 
         # Pass transforms in here, then run the next cell to see how the transforms look
-        trainset = torchvision.datasets.ImageNet(root='D:/Research/DL/Database/ImageNet/Val', split="train", download=True,
-                                                transform=transform_train)  # 训练数据集
+        trainset = torchvision.datasets.ImageNet(root='D:/Research/DL/Database/ImageNet/Val', split="train",
+                                                 download=True,
+                                                 transform=transform_train)  # 训练数据集
         train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True,
                                                    num_workers=1)  # 生成一个个batch进行批训练，组成batch的时候顺序打乱取
 
@@ -84,77 +129,59 @@ def make_data_loader(cfg, for_train):
         # cifar-10的标签
         classes_list = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
         return train_loader, val_loader, test_loader, classes_list
+    """
 
 
+def make_data_loader(cfg, for_train):
+    if cfg.DATA.DATASETS.NAMES == "none" or cfg.TRAIN.DATALOADER.IMS_PER_BATCH == 0:  #如果batch为0，那么就返回空  CJY at 2020.7.12
+        classes_list = ["{}".format(i) for i in range(cfg.MODEL.CLA_NUM_CLASSES)]
+        return None, None, None, classes_list
+
+    torchvision_dataset_list = ["cifar10", "cifar100", "pascal-voc", "coco"]
+    #custom_dataset_list = [""]
+
+    if cfg.DATA.DATASETS.NAMES in torchvision_dataset_list:
+        return make_data_loader_for_classic_datasets(cfg, for_train)
 
     train_transforms = build_transforms(cfg, is_train=for_train)
     val_transforms = build_transforms(cfg, is_train=False)
     test_transforms = build_transforms(cfg, is_train=False)
 
     num_workers = cfg.DATA.DATALOADER.NUM_WORKERS
-    if len(cfg.DATA.DATASETS.NAMES) == 1:
-        dataset = init_dataset(cfg.DATA.DATALOADER.NAMES, root=cfg.DATA.DATALOADER.ROOT_DIR)
-    else:
-        # TODO: add multi dataset to train
-        dataset = init_dataset(cfg.DATA.DATASETS.NAMES, root=cfg.DATA.DATASETS.ROOT_DIR)
-
-
+    dataset = init_dataset(cfg.DATA.DATASETS.NAMES, root=cfg.DATA.DATASETS.ROOT_DIR)
+    classes_list = dataset.category  # 建立classes_list
     num_classes = dataset.num_categories
-    # 建立classes_list
-    classes_list = dataset.category
 
-    #train set
-    #是否要进行label-smoothing
-    #train_set = ImageDataset(dataset.train, train_transforms)
+    #train set # ？？是否要进行label-smoothing
     train_set = ImageDataset(dataset.train, train_transforms)
-    if cfg.DATA.DATALOADER.SAMPLER == 'softmax':
-        """
-        train_loader = DataLoader(
-            train_set, batch_size=cfg.TRAIN.DATALOADER.IMS_PER_BATCH, shuffle=True, num_workers=num_workers,
-            collate_fn=train_collate_fn_classifaction
-        )
-        """
-        train_loader = DataLoader(
-            train_set, batch_size=cfg.TRAIN.DATALOADER.IMS_PER_BATCH,
-            sampler=RandomSampler(dataset.train, cfg.TRAIN.DATALOADER.CATEGORIES_PER_BATCH,
-                                  cfg.TRAIN.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, dataset.num_categories,
-                                  is_train=for_train),
-            # sampler=RandomIdentitySampler_alignedreid(dataset.train, cfg.DATALOADER.NUM_INSTANCE),      # new add by gu
-            num_workers=num_workers, collate_fn=collate_fn
-        )
-    else:
-        train_loader = DataLoader(
-            train_set, batch_size=cfg.TRAIN.DATALOADER.IMS_PER_BATCH,
-            sampler=RandomSampler(dataset.train, cfg.TRAIN.DATALOADER.CATEGORIES_PER_BATCH,
-                                  cfg.TRAIN.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, dataset.num_categories,
-                                  is_train=for_train),
-            # sampler=RandomIdentitySampler_alignedreid(dataset.train, cfg.DATALOADER.NUM_INSTANCE),      # new add by gu
-            num_workers=num_workers, collate_fn=collate_fn
-        )
+    train_loader = DataLoader(
+        train_set, batch_size=cfg.TRAIN.DATALOADER.IMS_PER_BATCH,
+        sampler=RandomSampler(dataset.train, cfg.TRAIN.DATALOADER.CATEGORIES_PER_BATCH,
+                              cfg.TRAIN.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, num_classes, is_train=for_train),
+        num_workers=num_workers, collate_fn=collate_fn
+    )
 
     #val set
-    #val_set = ImageDataset(dataset.val, val_transforms)
     val_set = ImageDataset(dataset.val, val_transforms)
     val_loader = DataLoader(
         val_set, batch_size=cfg.VAL.DATALOADER.IMS_PER_BATCH, shuffle=False, num_workers=num_workers,
         #CJY at 2019.9.26 为了能够平衡样本
-        sampler=RandomSampler(dataset.val, cfg.VAL.DATALOADER.CATEGORIES_PER_BATCH, cfg.VAL.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, dataset.num_categories, is_train=False),
+        sampler=RandomSampler(dataset.val, cfg.VAL.DATALOADER.CATEGORIES_PER_BATCH,
+                              cfg.VAL.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, num_classes, is_train=False),
         collate_fn=collate_fn
     )
 
     #test_set
-    #test_set = ImageDataset(dataset.test, test_transforms)
     test_set = ImageDataset(dataset.test, test_transforms)
     test_loader = DataLoader(
         test_set, batch_size=cfg.TEST.DATALOADER.IMS_PER_BATCH, shuffle=False, num_workers=num_workers,
         # CJY at 2019.9.26 为了能够平衡样本
-        sampler=RandomSampler(dataset.test, cfg.TEST.DATALOADER.CATEGORIES_PER_BATCH, cfg.TEST.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, dataset.num_categories, is_train=False),
+        sampler=RandomSampler(dataset.test, cfg.TEST.DATALOADER.CATEGORIES_PER_BATCH,
+                              cfg.TEST.DATALOADER.INSTANCES_PER_CATEGORY_IN_BATCH, num_classes, is_train=False),
         collate_fn=collate_fn
     )
     #notes:
     #1.collate_fn是自定义函数，对提取的batch做处理，例如分开image和label
-
-
     return train_loader, val_loader, test_loader, classes_list
 
 
@@ -172,28 +199,21 @@ def make_seg_data_loader(cfg, for_train):
     test_mask_transforms = build_seg_transforms(cfg, is_train=False, type="mask")
 
     num_workers = cfg.DATA.DATALOADER.NUM_WORKERS
-    if len(cfg.DATA.DATASETS.SEG_NAMES) == 1:
-        dataset = init_dataset(cfg.DATA.DATALOADER.NAMES, root=cfg.DATA.DATALOADER.ROOT_DIR)
-    else:
-        # TODO: add multi dataset to train
-        dataset = init_dataset(cfg.DATA.DATASETS.SEG_NAMES, root=cfg.DATA.DATASETS.ROOT_DIR)
-
-    # 建立classes_list
-    classes_list = dataset.category
+    dataset = init_dataset(cfg.DATA.DATASETS.SEG_NAMES, root=cfg.DATA.DATASETS.ROOT_DIR)
+    classes_list = dataset.category  # 建立classes_list
+    num_classes = dataset.num_categories
 
     # train set
     # 是否要进行label-smoothing
-    # train_set = ImageDataset(dataset.train, train_transforms)
     train_set = SegmentationDataset(dataset.seg_train, train_transforms, train_mask_transforms, cfg, is_train=for_train)
     train_loader = DataLoader(
         train_set, batch_size=cfg.TRAIN.DATALOADER.MASK_PER_BATCH, num_workers=num_workers, #shuffle=True,
         # CJY  为了保证类别均衡
-        sampler=RandomSamplerForSegmentation(dataset.seg_train, 1, 1, dataset.num_categories, is_train=for_train),    #此处让所有分类标签的按顺序以1为单位交替进行
+        sampler=RandomSamplerForSegmentation(dataset.seg_train, 1, 1, num_classes, is_train=for_train),    #此处让所有分类标签的按顺序以1为单位交替进行
         collate_fn=collate_fn_seg
     )
 
     # val set
-    # val_set = ImageDataset(dataset.val, val_transforms)
     val_set = SegmentationDataset(dataset.seg_val, val_transforms, val_mask_transforms, cfg, is_train=False)
     val_loader = DataLoader(
         val_set, batch_size=cfg.VAL.DATALOADER.MASK_PER_BATCH, shuffle=False, num_workers=num_workers,
@@ -201,7 +221,6 @@ def make_seg_data_loader(cfg, for_train):
     )
 
     # test_set
-    # test_set = ImageDataset(dataset.test, test_transforms)
     test_set = SegmentationDataset(dataset.seg_test, test_transforms, test_mask_transforms, cfg, is_train=False)
     test_loader = DataLoader(
         test_set, batch_size=cfg.TEST.DATALOADER.MASK_PER_BATCH, shuffle=False, num_workers=num_workers,
