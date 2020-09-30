d38230123b9968f61a0949a0fede7fa1eed3e28a
Jack <sychenjiayang@163.com>
2020-09-30 16:06:53 +0800

    1

--- data/build.py
+++ data/build.py
@@ -79,12 +79,32 @@ def make_data_loader_for_classic_datasets(cfg, for_train):
         root_path = os.path.join(root_path, "DATABASE", "Microsoft-COCO")
         annotation_path = os.path.join(root_path, "annotations")
         train_annfile =os.path.join(annotation_path, "captions_{}2017.json".format("train"))
+        train_set = torchvision.datasets.CocoDetection(root=root_path, annFile=train_annfile, transform=train_transforms)
+        val_annfile = os.path.join(annotation_path, "captions_{}2017.json".format("val"))
+        val_set = torchvision.datasets.CocoDetection(root=root_path, annFile=val_annfile, transform=val_transforms)
+        test_set = val_set
+        classes_list = None
+        num_classes = None
+    elif cfg.DATA.DATASETS.NAMES == "coco-caption":
+        root_path = os.path.join(root_path, "DATABASE", "Microsoft-COCO")
+        annotation_path = os.path.join(root_path, "annotations")
+        train_annfile =os.path.join(annotation_path, "captions_{}2017.json".format("train"))
         train_set = torchvision.datasets.CocoCaptions(root=root_path, annFile=train_annfile, transform=train_transforms)
         val_annfile = os.path.join(annotation_path, "captions_{}2017.json".format("val"))
         val_set = torchvision.datasets.CocoCaptions(root=root_path, annFile=val_annfile, transform=val_transforms)
         test_set = val_set
-        classes_list = train_set.classes
-        num_classes = len(classes_list)
+        classes_list = None
+        num_classes = None
+    elif cfg.DATA.DATASETS.NAMES == "coco-detection":
+        root_path = os.path.join(root_path, "DATABASE", "Microsoft-COCO")
+        annotation_path = os.path.join(root_path, "annotations")
+        train_annfile =os.path.join(annotation_path, "captions_{}2017.json".format("train"))
+        train_set = torchvision.datasets.CocoCaptions(root=root_path, annFile=train_annfile, transform=train_transforms)
+        val_annfile = os.path.join(annotation_path, "captions_{}2017.json".format("val"))
+        val_set = torchvision.datasets.CocoCaptions(root=root_path, annFile=val_annfile, transform=val_transforms)
+        test_set = val_set
+        classes_list = None
+        num_classes = None
 
     train_loader = DataLoader(
         train_set, batch_size=cfg.TRAIN.DATALOADER.IMS_PER_BATCH,
