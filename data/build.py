9892121f6ea645e1146779289b0a1af347bc746d
Jack <sychenjiayang@163.com>
2020-09-30 17:51:12 +0800

    1

--- data/build.py
+++ data/build.py
@@ -81,10 +81,10 @@ def make_data_loader_for_classic_datasets(cfg, for_train):
         annotation_path = os.path.join(root_path, "annotations")
         train_image_path = os.path.join(root_path, "{}{}".format("train", year))
         train_annfile =os.path.join(annotation_path, "instances_{}2017.json".format("train"))
-        train_set = torchvision.datasets.CocoDetection(root=train_image_path, annFile=train_annfile, transform=train_transforms)
+        train_set = torchvision.datasets.CocoClassification(root=train_image_path, annFile=train_annfile, transform=train_transforms)
         val_image_path = os.path.join(root_path, "{}{}".format("val", year))
         val_annfile = os.path.join(annotation_path, "instances_{}2017.json".format("val"))
-        val_set = torchvision.datasets.CocoDetection(root=val_image_path, annFile=val_annfile, transform=val_transforms)
+        val_set = torchvision.datasets.CocoClassification(root=val_image_path, annFile=val_annfile, transform=val_transforms)
         test_set = val_set
         classes_list = [train_set.coco.dataset["categories"][i]["name"] for i in range(len(train_set.coco.dataset["categories"]))]
         num_classes = len(classes_list)
