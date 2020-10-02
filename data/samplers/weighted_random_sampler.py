4e5cb2e34744feae8dd783ba45af8fe58f8de990
Jack <sychenjiayang@163.com>
2020-10-02 22:11:22 +0800

    1

--- data/samplers/weighted_random_sampler.py
+++ data/samplers/weighted_random_sampler.py
@@ -13,6 +13,7 @@ from torch.utils.data.sampler import WeightedRandomSampler
 #CJY at 2019.9.26
 class AutoWeightedRandomSampler(WeightedRandomSampler):
     def __init__(self, data_source, replacement=True):
-        super(AutoWeightedRandomSampler, self).__init__()
-        self.weights = [0, 1]
+        weights = [0.2, 0.4, 0.8, 1]
+        num_samples = 100
+        super(AutoWeightedRandomSampler, self).__init__(weights, num_samples)
         print(1)
