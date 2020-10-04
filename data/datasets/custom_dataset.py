2d7e05c0367c778f07cd53d4547e628fa1bfe087
Jack <sychenjiayang@163.com>
2020-10-04 17:17:34 +0800

    1

--- data/datasets/custom_dataset.py
+++ data/datasets/custom_dataset.py
@@ -183,7 +183,7 @@ class DDRColormaskDataset(Dataset):
         mask = []
         origin_mask = []
         img_numpy = np.asarray(img_pil)
-        img_numpy_nonzero = np.sum(img_numpy, axis=0, keepdim=True) > 0
+        img_numpy_nonzero = np.sum(img_numpy, axis=2, keepdims=True) > 0
         for mask_p in mask_path:
             mask_pil = Image.open(mask_p)
             mask_numpy = np.asarray(mask_pil)
