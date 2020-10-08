cb24185026ba88069ebf2bf6d59ae2ffa82eb445
Jack <sychenjiayang@163.com>
2020-10-08 17:10:51 +0800

    1

--- modeling/visualizers/cjy_cam_screen.py
+++ modeling/visualizers/cjy_cam_screen.py
@@ -415,7 +415,8 @@ class CJY_CAM_SCREEN():
         #gcam = torch.sum(inter_gradient * inter_output, dim=1, keepdim=True)
         #gcam = gcam * (gcam.shape[-1] * gcam.shape[-2])  # 如此，形式上与最后一层计算的gcam量级就相同了  （由于最后loss使用mean，所以此处就不mean了）
         gcam = torch.sum(inter_gradient * inter_output, dim=1, keepdim=True)
-
+        if self.reservePos == True:
+            gcam = torch.relu(gcam)  # CJY at 2020.4.18
         return gcam
 
     # Generate Overall CAM
