df8f2fb4717149f9a56fc563c11c4fd519c58b50
Jack <sychenjiayang@163.com>
2020-10-09 14:01:27 +0800

    1

--- engine/visualizer.py
+++ engine/visualizer.py
@@ -175,14 +175,14 @@ def create_supervised_visualizer(model, metrics, loss_fn, device=None):
                 # PASCAL-VOC
                 num_labels = labels.gt(0).sum().item()
                 s_labels = torch.sort(labels, dim=1, descending=True)[1][:, 0:num_labels]
-                s_p_labels = torch.sort(logits, dim=1, descending=True)[1][:, 0:num_labels+num_th]
+                s_p_labels = torch.sort(logits, dim=1, descending=True)[1][0:s_labels.shape[0], 0:num_labels+num_th]
                 o_labels = torch.cat([s_labels, s_p_labels], dim=1)
                 oblabelList = [o_labels[:, j] for j in range(o_labels.shape[1])]
             elif model.num_classes == 80 and model.classifier_output_type == "multi-label":
                 # PASCAL-VOC
                 num_labels = labels.gt(0).sum().item()
                 s_labels = torch.sort(labels, dim=1, descending=True)[1][:, 0:num_labels]
-                s_p_labels = torch.sort(logits, dim=1, descending=True)[1][:, 0:num_labels + num_th]
+                s_p_labels = torch.sort(logits, dim=1, descending=True)[1][0:s_labels.shape[0], 0:num_labels + num_th]
                 o_labels = torch.cat([s_labels, s_p_labels], dim=1)
                 oblabelList = [o_labels[:, j] for j in range(o_labels.shape[1])]
             elif model.num_classes == 1000:
