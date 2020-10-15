a3dbddf9f1086374cf914ab1c57b54201fe31589
Jack <sychenjiayang@163.com>
2020-10-16 00:31:54 +0800

    1

--- modeling/visualizers/cjy_contrast_guided_backpropagation.py
+++ modeling/visualizers/cjy_contrast_guided_backpropagation.py
@@ -302,8 +302,8 @@ class CJY_CONTRAST_GUIDED_BACKPROPAGATION():
             #3. consider negtive
             if relu_output.ndimension() == 2:
                 new_grad_in0 = grad_in_sub[0] + grad_in_sub[1] * grad_in_sub[1].lt(0).float() - grad_in_sub[2] * grad_in_sub[2].lt(0).float()
-                new_grad_in1 = grad_in_sub[1] * grad_in_sub[1].gt(0).float()
-                new_grad_in2 = grad_in_sub[2] * grad_in_sub[2].gt(0).float()
+                new_grad_in1 = grad_in_sub[1] * grad_in_sub[1].gt(0).float() - grad_in_sub[2] * grad_in_sub[2].lt(0).float()
+                new_grad_in2 = grad_in_sub[2] * grad_in_sub[2].gt(0).float() - grad_in_sub[1] * grad_in_sub[1].lt(0).float()
 
                 new_grad_in_sub = [new_grad_in0, new_grad_in1, new_grad_in2]
                 new_grad_in = torch.cat(new_grad_in_sub, dim=0)
