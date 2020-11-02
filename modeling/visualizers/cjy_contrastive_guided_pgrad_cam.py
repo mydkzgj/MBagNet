70664ab400da04c7633e5666384a5d72e39dcb43
Jack <sychenjiayang@163.com>
2020-11-02 23:29:41 +0800

    1

--- modeling/visualizers/cjy_contrastive_guided_pgrad_cam.py
+++ modeling/visualizers/cjy_contrastive_guided_pgrad_cam.py
@@ -531,7 +531,10 @@ class CJY_CONTRASTIVE_GUIDED_PGRAD_CAM():
         inter_output_sub = [inter_output[i * num_sub_batch: (i + 1) * num_sub_batch] for i in range(self.multiply_input)]
         inter_gradient_sub = [inter_gradient[i * num_sub_batch: (i + 1) * num_sub_batch] for i in range(self.multiply_input)]
 
-        gcam_all = torch.sum((inter_output * inter_gradient).relu(), dim=1, keepdim=True)
+        if self.guided_type == "grad":
+            gcam_all = torch.sum((inter_output * inter_gradient).relu(), dim=1, keepdim=True)
+        elif self.guided_type == "cam":
+            gcam_all = torch.sum((inter_output * inter_gradient), dim=1, keepdim=True)
         """
         if inter_output.min() < 0:
             gcam_all = torch.sum(inter_output * inter_gradient, dim=1, keepdim=True)
