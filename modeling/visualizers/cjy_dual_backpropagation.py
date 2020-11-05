f7aa02ab52ddca2f1f07d65f64733112a45a06cf
Jack <sychenjiayang@163.com>
2020-11-05 11:38:08 +0800

    1

--- modeling/visualizers/cjy_dual_backpropagation.py
+++ modeling/visualizers/cjy_dual_backpropagation.py
@@ -393,7 +393,7 @@ class CJY_DUAL_BACKPROPAGATION():
                 # """
                 # 2.使用主干的cam限制分支的cam范围
                 current_cam = self.GenerateCAM(relu_output, grad_out[0]).gt(0).float()
-                if self.CAM.shape[-1] != current_cam.shape and self.CAM is not 1:
+                if self.CAM is not 1 and self.CAM.shape[-1] != current_cam.shape:
                     self.CAM = torch.nn.functional.interpolate(self.CAM, (current_cam.shape[2], current_cam.shape[3]),
                                                                mode='nearest')
                 cam = self.CAM * current_cam
@@ -442,9 +442,8 @@ class CJY_DUAL_BACKPROPAGATION():
                 # """
                 # 2.使用主干的cam限制分支的cam范围
                 current_cam = self.GenerateCAM(maxpool_output, grad_out[0]).gt(0).float()
-                if self.CAM.shape[-1] != current_cam.shape and self.CAM is not 1:
-                    self.CAM = torch.nn.functional.interpolate(self.CAM, (current_cam.shape[2], current_cam.shape[3]),
-                                                               mode='nearest')
+                if self.CAM is not 1 and self.CAM.shape[-1] != current_cam.shape:
+                    self.CAM = torch.nn.functional.interpolate(self.CAM, (current_cam.shape[2], current_cam.shape[3]), mode='nearest')
                 cam = self.CAM * current_cam
                 if self.relu_output_obtain_index in self.stem_relu_index_list:
                     self.CAM = cam
